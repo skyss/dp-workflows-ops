6dcdede7b6ae1ea2672bef1ec56613c049bdb482
John Erik Sloper <john.erik.sloper@vlfk.no>
2025-09-22 14:26:48 +0200

    Also take dbutils as parameter

--- src/dp_workflow_ops/job_setup.py
+++ src/dp_workflow_ops/job_setup.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 if TYPE_CHECKING:
     from pyspark.sql import SparkSession
 
 
-def set_catalog_and_schema_for_job(spark: SparkSession) -> None:
+def set_catalog_and_schema_for_job(spark: SparkSession, dbutils: Any) -> None:
     """Retrives catallog and schema from job parameters and sets catalog and schema.
 
     If schema does not exist, it is created."""
