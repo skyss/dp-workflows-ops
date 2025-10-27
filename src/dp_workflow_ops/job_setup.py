f29c18cad48b52065baa158f7ac71a7a6747262e
John Erik Sloper <john.erik.sloper@skyss.no>
2025-10-27 12:16:14 +0100

    Return catalog and schema from setup function

--- src/dp_workflow_ops/job_setup.py
+++ src/dp_workflow_ops/job_setup.py
@@ -9,13 +9,22 @@ if TYPE_CHECKING:
     from pyspark.sql import SparkSession
 
 
-def set_catalog_and_schema_for_job(spark: SparkSession, dbutils: RemoteDbUtils) -> None:
-    """Retrives catallog and schema from job parameters and sets catalog and schema.
+def set_catalog_and_schema_for_job(
+    spark: SparkSession, dbutils: RemoteDbUtils
+) -> tuple[str, str]:
+    """Retrives catalog and schema from job parameters and sets catalog and schema.
 
     If schema does not exist, it is created.
+
+    Returns
+    -------
+    tuple: tuple[str,str]
+        Catalog and schema name
+
     """
     catalog = dbutils.widgets.get("catalog")
     schema = dbutils.widgets.get("schema")
     spark.sql(f"create schema if not exists `{catalog}`.`{schema}`")
     spark.sql(f"use catalog `{catalog}`")
     spark.sql(f"use schema `{schema}`")
+    return catalog, schema
